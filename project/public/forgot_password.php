bc67ea6e19baff8421d7a3a0e0ea99d96ab54c26
AlexMenezes <p2724348@my365.dmu.ac.uk>
2025-05-17 17:38:13 +0100

    updated css and some bugs

--- project/public/forgot_password.php
+++ project/public/forgot_password.php
@@ -6,18 +6,20 @@ require '../includes/header.php';
 <head>
     <meta charset="utf-8">
     <title>Forgot Password - Grey Lock</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/signup.css"> 
+    <link rel="stylesheet" type="text/css" href="assets/css/forgot_password.css">
+    <script src="assets/js/darkmode.js"></script>
 </head>
 <body>
-    <div class="content">
+    <div class="forgot-container">
         <h2>Reset Your Password</h2>
         <form action="../includes/forgot_password.inc.php" method="POST">
             <label for="email">Enter your email address:</label>
             <input type="email" id="email" name="email" required>
             <br><br>
             <button type="submit">Send Reset Link</button>
+            <p>Remembered your password? <a href="/websites/GreyLock/Web-Based-Password-Manager-GreyLock/project/public/login">Login</a></p>
+
         </form>
-        <p>Remembered your password? <a href="/websites/GreyLock/Web-Based-Password-Manager-GreyLock/project/public/login">Login</a></p>
     </div>
     <footer>
         &copy; 2025 Grey Lock
